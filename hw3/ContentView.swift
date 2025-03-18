6a3a16418bc77eb2708a34e12085b911761bd1d6
anikamadan <110501191+anikamadan@users.noreply.github.com>
2025-03-18 18:27:52 -0400

    Initial Commit

---
+++ hw3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hw3
+//
+//  Created by Anika Madan on 3/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hw3/hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  hw3App.swift
+//  hw3
+//
+//  Created by Anika Madan on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
