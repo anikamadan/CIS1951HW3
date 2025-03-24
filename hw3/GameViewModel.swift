7e3d1a43dad92369cf27690ba121c2ceb996d4ac
sophie2003cs <sophiexu@sas.upenn.edu>
2025-03-23 23:00:02 -0400

    included a startGame() function that allows us to process motion management in the background (hopefully we'll find a way to trigger the startGame() function in our UI)

--- hw3/GameViewModel.swift
+++ hw3/GameViewModel.swift
@@ -10,6 +10,7 @@ import SwiftUI
 import Observation
 import CoreLocation
 import CoreMotion
+import MapKit
 
 // not sure if we should include a "Did Update Locations" method or not?
 // not sure how to request location permissions + handle the case where the user has already approved our request +
@@ -53,6 +54,12 @@ import CoreMotion
         print("Failed to get location: \(error)")
     }
     
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//     if let location = locations.last {
+//         send(location, to: )
+//     }
+//    }
+    
     // method that checks if we are within 50 meters of any dining hall
     func checkDistanceFromAnyHall(currentLocation: CLLocation) {
         for index in 0..<diningHallModel.diningHalls.count {
@@ -60,10 +67,36 @@ import CoreMotion
             let diningHallLocation = CLLocation(latitude: diningHall.location.latitude, longitude: diningHall.location.longitude)
             let distance = currentLocation.distance(from: diningHallLocation)
             if distance <= 50 && !diningHall.isCollected {
+                feedbackGenerator.notificationOccurred(.success)
                 diningHall.completeHall()
                 diningHallModel.collectedHalls.append(diningHall)
             }
         }
+    }
+    
+    func startGame() {
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 1 / 50
+            motionManager.startDeviceMotionUpdates(to: .main) { [weak self] motion, error in
+                if let self {
+                    if let motion {
+                        handleMotion(motion)
+                    } else if let error {
+                        print("Failed to receive motion update: \(error)")
+                        motionManager.stopDeviceMotionUpdates()
+                    }
+                }
+            }
+        } else {
+            print("Device motion is not available!")
+        }
+    }
+    
+    func handleMotion(_ motion: CMDeviceMotion) {
+        let correctThreshold = Double.pi * 0.35
+        let incorrectThreshold = Double.pi * 0.65
+        let absoluteRoll = abs(motion.attitude.roll)
+        
     }
  //    func locationManager (
  //        _ manager: CLLocationManager,
